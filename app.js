509ded016ce3c8dc2e0eaa79314f115dc9711406
mel <mchuam@gmail.com>
2018-04-17 22:16:52 +0000

    Committed first try at Express and EJS

---
+++ app.js
@@ -0,0 +1,38 @@
+// creating first express server with routing
+const express = require("express");
+const app = express(); // does this run it here?
+const PORT = process.env.PORT || 8080; // default 8080
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on ${PORT}!`);
+});
