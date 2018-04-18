96c1644634e45d7c6b5348d8ac238660da987930
mel <mchuam@gmail.com>
2018-04-18 02:11:43 +0000

    last changes for the day

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString(numberOfChars) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  for (var i = 0; i < numberOfChars; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return `${text}`;
+}
+
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -38,6 +50,23 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  var shortRandomURL = generateRandomString(6);
+  if (req.body.longURL.slice(7) === 'http://') {
+    urlDatabase[shortRandomURL] = req.body.longURL;
+  } else {
+    urlDatabase[shortRandomURL] = 'http://' + req.body.longURL;
+  }
+  console.log(urlDatabase);
+  res.redirect(302, `/urls/${shortRandomURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on ${PORT}!`);
